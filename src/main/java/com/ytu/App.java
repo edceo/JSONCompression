9e8c1f05e5a059695819a185e5c6bb7c2738bb5c
edceo <yusufonder_55@hotmail.com>
2018-01-17 12:17:27 +0300

    Main Method güncellendi

--- src/main/java/com/ytu/App.java
+++ src/main/java/com/ytu/App.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 public class App {
 
     private static final String DIR_NAME = "/Users/yusufonder/IdeaProjects/JSONCompression/src/main/resources/";
-    private static final Path JSON_FILE_NAME = Paths.get(DIR_NAME + "raw.json");
+    private static final Path JSON_FILE_NAME = Paths.get(DIR_NAME + "nodejs.json");
     private static final Path ZSTD_FILE_NAME = Paths.get(DIR_NAME + "comp.zstd");
     private static final Path GZIP_FILE_NAME = Paths.get(DIR_NAME + "comp.gzip");
     private static final Path COMP_FILE_NAME = Paths.get(DIR_NAME + "comp.json");
@@ -23,14 +23,16 @@ public class App {
     public static void main(String[] args) throws IOException {
         String lines = FileUtil.readFileLines(JSON_FILE_NAME.toFile());
         String pack = JSONCompress.pack(StringUtils.trim(lines).replace("\n", ""));
+        System.out.println(pack);
         FileUtil.writeFile(COMP_FILE_NAME.toFile(), pack.getBytes());
-        long json = JSON_FILE_NAME.toFile().length() / 1024;
+        long json = JSON_FILE_NAME.toFile().length();
         System.out.println("ORIGINAL FILE LENGTH -> " + json);
-        long comp = COMP_FILE_NAME.toFile().length() / 1024;
+        long comp = COMP_FILE_NAME.toFile().length();
         System.out.println("COMP FILE LENGTH -> " + comp);
         zstd(pack);
         gzip(pack);
-        //System.out.println(JSONCompress.unpack(pack));
+        Object unpack = JSONCompress.unpack(pack);
+        System.out.println(unpack);
 
         System.out.println("RATIO -> " + comp / (double)json);
 
@@ -40,13 +42,13 @@ public class App {
 
         byte[] compress = Zstd.compress(pack.getBytes());
         FileUtil.writeFile(ZSTD_FILE_NAME.toFile(), compress);
-        System.out.println("ZSTD FILE LENGTH -> " + ZSTD_FILE_NAME.toFile().length() / 1024);
+        System.out.println("ZSTD FILE LENGTH -> " + ZSTD_FILE_NAME.toFile().length());
         byte[] decompress = Zstd.decompress(compress, (int) Zstd.decompressedSize(compress));
     }
 
     private static void gzip(String pack) throws IOException {
         FileUtil.gzipComp(pack, GZIP_FILE_NAME.toFile());
-        System.out.println("GZIP FILE LENGTH -> " + GZIP_FILE_NAME.toFile().length() / 1024);
+        System.out.println("GZIP FILE LENGTH -> " + GZIP_FILE_NAME.toFile().length());
         String s = FileUtil.gzipDecomp(GZIP_FILE_NAME.toFile());
 
     }
